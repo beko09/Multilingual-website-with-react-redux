ad7d027c62258be6e76748f960711913a2cd42ba
beko09 <abobakerhlal09@gmail.com>
2020-06-03 16:16:41 -0700

    edit style links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,11 +3,13 @@ export default function footer() {
     return (
       <div className="footer">
         <p>
+          Made by <a href="http://www.beko4learn.com">Abobaker Hilal</a>
+        </p>
+        <p>
           <a href="https://github.com/beko09/Multilingual-website-with-react-redux">
             github
           </a>
         </p>
-        Made by <a href="http://www.beko4learn.com">Abobaker Hilal</a>
       </div>
     );
 }
