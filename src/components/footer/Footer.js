7a62c9ecac5adb5118000f24c59a6efcce910c67
abobaker hilal <38497282+beko09@users.noreply.github.com>
2022-03-15 13:35:20 +0200

    update link footer
    update link primary web site

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ export default function footer() {
     return (
       <div className="footer">
         <p>
-          Made by <a href="http://www.beko4learn.com">Abobaker Hilal</a>
+          Made by <a href="https://d-s.sd">Abobaker Hilal</a>
         </p>
         <p>
           <a href="https://github.com/beko09/Multilingual-website-with-react-redux">
